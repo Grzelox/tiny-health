f3c479ef848ca6020147b6eea4bb7e642aa0946a
Wojtek <wojtek.grzelczyk@gmail.com>
2025-01-30 00:06:36 +0100

    feat: add auth to api routes

--- src/app/api/getPets/route.ts
+++ src/app/api/getPets/route.ts
@@ -1,17 +1,25 @@
+import { createClient } from "@/utils/supabase/server";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
-  const { userId } = auth();
-  if (!userId) {
+  const supabase = await createClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const user = session?.user;
+  if (!user) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   const searchParams = new URL(request.url).searchParams;
   const ownerId = searchParams.get("ownerId");
   if (!ownerId) {
-    return NextResponse.json({ error: "Owner ID is required" }, { status: 400 });
+    return NextResponse.json(
+      { error: "Owner ID is required" },
+      { status: 400 },
+    );
   }
   try {
     const pets = await prisma.pet.findMany({
--- src/middleware.ts
+++ src/middleware.ts
@@ -47,7 +47,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
+     * - api routes
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
